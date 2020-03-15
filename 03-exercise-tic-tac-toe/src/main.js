84042fada86e9f037c3dd6de63f92b1b6f9ac3fe
Vignesh Shankar <vignesh.shankar@solitontech.local>
2020-03-15 14:24:54 +0530

    Implement initial displayConsroller logic
    Signed-off-by: Vignesh Shankar <vignesh.shankar@solitontech.local>

--- 03-exercise-tic-tac-toe/src/main.js
+++ 03-exercise-tic-tac-toe/src/main.js
@@ -1,3 +1,38 @@
+const displayController = (
+    function() {
+
+        const render = function() {
+
+            const _array = gameBoard.getArray();
+
+            const gameBoardInterface = document.querySelector('game-board');
+            while (gameBoardInterface.firstChild) {
+                gameBoardInterface.removeChild(gameBoardInterface.lastChild);
+            }
+
+            for (let row = 0; row < _array.length; row++) {
+                for (let column = 0; column < _array[row].length; column++) {
+                    const cell = document.createElement('div');
+                    cell.setAttribute('class', 'cell');
+                    cell.setAttribute('row', row);
+                    cell.setAttribute('column', column);
+                    cell.addEventListener('click', function(event) {
+                        const _row = event.target.getAttribute('row');
+                        const _column = event.target.getAttribute('column');
+                        const currentPlayer = gameBoard.getCurrentPlayer();
+                        gameBoard.registerMove(currentPlayer, _row, _column);
+                        event.target.style.backgroundColor = currentPlayer.color;
+                    })
+                    gameBoardInterface.appendChild(cell);
+                }
+                const cellBreak = document.createElement('br');
+                gameBoardInterface.appendChild(cellBreak);
+            }
+        }
+        return { render };
+    }
+)();
+
 const gameBoard = (
     function() {
         const MAX_PLAYERS = 2;
@@ -6,7 +41,7 @@ const gameBoard = (
         let playersList = [];
         let currentPlayer;
 
-        const _array = [
+        let _array = [
             [-1, -1, -1],
             [-1, -1, -1],
             [-1, -1, -1]
@@ -28,8 +63,11 @@ const gameBoard = (
                 return;
             }
             _array[row][column] = player.id;
-            isCurrentPlayerWinner = decideIfCurrentPlayerIsWinner();
-            if (!isCurrentPlayerWinner) {
+            if (checkIfWinner(currentPlayer)) {
+                console.log(currentPlayer.getName() + ' won!!');
+                reset();
+                displayController.render();
+            } else {
                 const nextPlayerId = currentPlayer.id == MAX_PLAYERS - 1 ? 0 : currentPlayer.id + 1;
                 currentPlayer = playersList[nextPlayerId];
                 console.log('Next player: ' + currentPlayer.getName());
@@ -37,19 +75,30 @@ const gameBoard = (
             }
         };
 
-        const decideIfCurrentPlayerIsWinner = function() {
-            const indices = getIndicesForId(currentPlayer.id);
-            console.group('Current Player: ' + currentPlayer.getName());
-                console.log('ID: ' + currentPlayer.id);
-                console.log('Board: '+ JSON.stringify(gameBoard.getArray()));;
-                console.log('Found indices: ' + JSON.stringify(indices));
-            console.groupEnd();
+        const checkIfWinner = function(currentPlayer) {
+            const winPossibilities = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8],    // Row wins
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],    // Column wins
+                [0, 4, 8], [2, 4, 6]                // Diagonal wins
+            ];
+
+            const flat_array = _array.flat(1);
+            // Check all possible winning combinations
+            for (i = 0; i < flat_array.length-1; i++) {
+                if (
+                    flat_array[winPossibilities[i][0]] === currentPlayer.id &&
+                    flat_array[winPossibilities[i][1]] === currentPlayer.id &&
+                    flat_array[winPossibilities[i][2]] === currentPlayer.id
+                ) {
+                    return true; 
+                }
+            } 
             return false;
         };
 
         const getIndicesForId = function(id) {
             const indices = [];
-            for (row = 0; row <_array.length; row++) {
+            for (row = 0; row < _array.length; row++) {
                 for (column = 0; column <_array[row].length; column++) {
                     if (_array[row][column] === id) {
                         indices.push([row, column]);
@@ -63,37 +112,37 @@ const gameBoard = (
             return _array;
         };
 
+        const getCurrentPlayer = function() {
+            return currentPlayer;
+        }
+
         const reset = function() {
-            playersList = [];
+            _array = [
+                [-1, -1, -1],
+                [-1, -1, -1],
+                [-1, -1, -1]
+            ];
         };
 
-        return { registerPlayer,getArray, reset, registerMove };
+        return { registerPlayer, getArray, reset, registerMove, getCurrentPlayer };
     }
 )();
 
-const displayController = (
-    function() {
-        return {};
-    }
-)();
-
-const Player = function(name) {
+const Player = function(name, color) {
     const getName  = function() {
         return name;
     }
     const id = -1;
-    return { getName, id };
+    return { getName, id , color };
 };
+
 
 gameBoard.reset();
 
-playerZero = Player('Player Zero');
-playerOne = Player('Player One');
+playerZero = Player('Player Zero', 'green');
+playerOne = Player('Player One', 'red');
 
 gameBoard.registerPlayer(playerZero);
 gameBoard.registerPlayer(playerOne);
 
-gameBoard.registerMove(playerZero, 0, 0);
-gameBoard.registerMove(playerOne, 1, 1);
-gameBoard.registerMove(playerZero, 0, 1);
-gameBoard.registerMove(playerOne, 1, 0);
+displayController.render();
