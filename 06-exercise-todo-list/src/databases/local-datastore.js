74e18f6c07f5dc1b33313cdb80cb4d60dd0f4ff1
Vignesh <vignesh.shankar@solitontech.com>
2020-04-04 22:30:39 +0530

    Create a module to store todoList tasks in localStorage usin store-js under the hood
    Signed-off-by: Vignesh <vignesh.shankar@solitontech.com>

---
+++ 06-exercise-todo-list/src/databases/local-datastore.js
@@ -0,0 +1,121 @@
+import store from 'store-js';
+
+class LocalStorage {
+
+    constructor() {
+        this.emptyTodoListDatastore = {
+            taskDatastore: [],
+            projectDatastore: []
+        }
+    }
+
+    clearTodoListDatastore() {
+        this.todoListDatastore = this.emptyTodoListDatastore;
+    }
+
+    get todoListDatastore() {
+        return store.get('todoListDatastore') || this.emptyTodoListDatastore;
+    }
+
+    set todoListDatastore(value) {
+        store.set('todoListDatastore', value);
+    }
+
+    get taskDatastore() {
+        return this.todoListDatastore.taskDatastore;
+    }
+
+    set taskDatastore(value) {
+        const _todoListDatastore = this.todoListDatastore;
+        _todoListDatastore.taskDatastore = value;
+        this.todoListDatastore = _todoListDatastore;
+    }
+
+    get projectDatastore() {
+        return this.todoListDatastore.projectDatastore;
+    }
+
+    set projectDatastore(value) {
+        const _todoListDatastore = this.todoListDatastore;
+        _todoListDatastore.projectDatastore = value;
+        this.todoListDatastore = _todoListDatastore;
+    }
+}
+
+
+const Datastore = (function() {
+    const localStorage = new LocalStorage();
+    localStorage.clearTodoListDatastore();
+
+    const _generateUUID = function() {
+        return 'xxyxxxy'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    function createTask(object) {
+        object.id = _generateUUID();
+        const _taskDatastore = localStorage.taskDatastore;
+        _taskDatastore.push(object);
+        localStorage.taskDatastore = _taskDatastore;
+        return object;
+    };
+
+    function readAllTasks() {
+        return localStorage.taskDatastore;
+    };
+
+    function updateTask(object) {
+        const taskId = object.id;
+        const _taskDatastore = localStorage.taskDatastore;
+        const objectIndex = _taskDatastore.findIndex((
+            _object => _object.id === taskId
+        ))
+        _taskDatastore[objectIndex] = object;
+        localStorage.taskDatastore = _taskDatastore;
+        return object;
+    };
+
+    function deleteTask(taskId) {
+        localStorage.taskDatastore = localStorage.taskDatastore.filter((
+            _object => _object.id !== taskId
+        ));
+    };
+
+    function createProject(object) {
+        object.id = _generateUUID();
+        const _projectDatastore = localStorage.projectDatastore;
+        _projectDatastore.push(object);
+        localStorage.projectDatastore = _projectDatastore;
+        return object;
+    };
+
+    function readAllProjects() {
+        return localStorage.projectDatastore;
+    };
+
+    function updateProject(object) {
+        const projectId = object.id;
+        const _projectDatastore = localStorage.projectDatastore;
+        const objectIndex = _projectDatastore.findIndex((
+            _object => _object.id === projectId
+        ))
+        _projectDatastore[objectIndex] = object;
+        localStorage.projectDatastore = _projectDatastore;
+        return object;
+    };
+
+    function deleteProject(projectId) {
+        localStorage.projectDatastore = localStorage.projectDatastore.filter((
+            _object => _object.id !== projectId
+        ));
+    };
+
+    return {
+        createTask, readAllTasks, updateTask, deleteTask,
+        createProject, readAllProjects, updateProject, deleteProject
+    };
+})();
+
+export { Datastore };
--- 06-exercise-todo-list/src/index.js
+++ 06-exercise-todo-list/src/index.js
@@ -1,26 +1,30 @@
-import { Datastore } from "./databases/firebase-datastore";
+import { Datastore } from "./databases/local-datastore";
 import { DomController } from "./views/dom-conrol";
 
 const content = document.querySelector('#content');
 content.appendChild(DomController.render());
 
-let allTasks = [];
-let allProjects = [];
+Datastore.readAllProjects().forEach(project => {
+    DomController.addToProjectsList(project);
+});
+Datastore.readAllTasks().forEach(task => {
+    DomController.addToTasksList(task);
+});
 
-allTasks = Datastore.readAllTasks();
-allProjects = Datastore.readAllProjects();
+const newProject = Datastore.createProject({
+    'title': 'Personal'
+});
+DomController.addToProjectsList(newProject);
 
-// const newProject = Datastore.createProject({
-//     'title': 'Personal'
-// });
-// DomController.addToProjectsList(newProject);
+const newTask = Datastore.createTask({
+    'description': 'Use GPay to complete the payment for FEB 2020',
+    'dueDate': '08 MAR 2020',
+    'pending': true,
+    'priority': 'High',
+    'projectId': newProject.id,
+    'title': 'Pay Electricity bill'
+});
+DomController.addToTasksList(newTask);
 
-// const newTask = Datastore.createTask({
-//     'description': 'Use GPay to complete the payment for FEB 2020',
-//     'dueDate': '08 MAR 2020',
-//     'pending': true,
-//     'priority': 'High',
-//     'projectId': newProject.id,
-//     'title': 'Pay Electricity bill'
-// });
-// DomController.addToTasksList(newTask);
+console.log(Datastore.readAllTasks());
+console.log(Datastore.readAllProjects());
