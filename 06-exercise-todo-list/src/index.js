b6cdc8263fb9c06c229fc840e7ab8afaac42f34c
Vignesh <vignesh.shankar@solitontech.com>
2020-04-05 19:11:49 +0530

    MOve code from dom-control to index
    Signed-off-by: Vignesh <vignesh.shankar@solitontech.com>

--- 06-exercise-todo-list/src/index.js
+++ 06-exercise-todo-list/src/index.js
@@ -1,2 +1,32 @@
 
 import { DomController } from "./views/dom-conrol";
+import { Datastore } from "./databases/local-datastore";
+
+const content = document.getElementById('content');
+content.appendChild(DomController.render());
+
+Datastore.readAllProjects().forEach(projectObject => {
+    const projectDOM = DomController.convertProjectObjectToDOM(projectObject);
+    DomController.addToProjectDOMList(projectDOM);
+});
+Datastore.readAllTasks().forEach(taskObject => {
+    const taskDOM = DomController.convertTaskObjectToDOM(taskObject);
+    DomController.addToTaskDOMList(taskDOM);
+});
+
+const newProjectObject = Datastore.createProject({
+    'title': 'Personal'
+});
+const newProjectDOM = DomController.convertProjectObjectToDOM(newProjectObject);
+DomController.addToProjectDOMList(newProjectDOM);
+
+const newTaskObject = Datastore.createTask({
+    'description': 'Use GPay to complete the payment for FEB 2020',
+    'dueDate': '08 MAR 2020',
+    'completed': false,
+    'priority': 'High',
+    'projectId': newProjectObject.id,
+    'title': 'Pay Electricity bill'
+});
+const newTaskDOM = DomController.convertTaskObjectToDOM(newTaskObject);
+DomController.addToTaskDOMList(newTaskDOM);
